ad7019fb7815daf673e108a6dcf4fb4760dc0432
MrGallo <daniel.gallo@ycdsbk12.ca>
2018-03-06 09:28:55 -0500

    Add Arrays review

---
+++ Arrays/Arrays.java
@@ -0,0 +1,43 @@
+public class Arrays {
+    public static void main(String[] args) {
+//        String[] students = new String[3];
+//        students[0] = "Jim";
+//        students[1] = "Tom";
+//        students[2] = "Kayla";
+
+        String[] students = {"Jim", "Tom", "Kayla"};
+        students[1] = "Mr. Skeleton";
+
+        printStudents(students);
+
+        int[][] marks = {
+                {50, 68, 87},
+                {97, 86, 56},
+                {90, 87, 99}
+        };
+
+        for (int i = 0; i < marks.length; i++) {
+            int[] markList = marks[i];
+            System.out.println(students[i] + "'s marks");
+            for (int mark : markList) {
+                System.out.println("mark = " + mark);
+            }
+        }
+
+        String[] tempArray = new String[students.length + 1];
+        for (int i = 0; i < students.length; i++) {
+            tempArray[i] = students[i];
+        }
+        tempArray[3] = "Donald";
+
+        students = tempArray;
+        printStudents(students);
+
+    }
+
+    private static void printStudents(String[] students) {
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("students[" + i + "] = " + students[i]);
+        }
+    }
+}
